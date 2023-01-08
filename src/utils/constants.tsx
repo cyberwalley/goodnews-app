8bab4a0c77b4a58df44e5e491f2a159482c5bd52
Olawale Oshinowo <olawaleoshinowo@olawales-mbp-2.lan>
2023-01-08 15:12:41 -0500

    added footer

---
+++ src/utils/constants.tsx
@@ -0,0 +1,23 @@
+export const SITE_NAME = 'getdailyoffers.com';
+export const SITE_URL = 'https://getdailyoffers.com';
+export const SITE_DESCRIPTION = 'Get Daily Offers';
+export const SITE_KEYWORDS = 'getdailyoffers';
+export const SITE_IMAGE = 'https://getdailyoffers.com/images/logo.png';
+export const SITE_AUTHOR = 'getdailyoffers';
+export const SITE_LANGUAGE = 'en';
+export const SITE_LOCALE = 'en_US';
+export const SITE_TWITTER = '@getdailyoffers';
+export const SITE_FACEBOOK = 'getdailyoffers';
+export const SITE_INSTAGRAM = 'getdailyoffers';
+export const SITE_GOOGLE = 'getdailyoffers';
+export const SITE_YOUTUBE = 'getdailyoffers';
+export const SITE_GITHUB = 'getdailyoffers';
+export const SITE_LINKEDIN = 'getdailyoffers';
+export const SITE_EMAIL = '';
+export const SITE_PHONE = '';
+export const SITE_ADDRESS = '';
+export const SITE_FAVICON = '/images/favicon.png';
+export const SITE_LOGO = 'https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600';
+export const SITE_LOGO_TEXT = '/images/logo-text.png';
+
+
