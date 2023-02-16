83bc6ae146f4d7f758b10826547fa43d649d3214
cyberwalley <walleyads@agmail.com>
2023-02-16 00:05:56 -0500

    add functions

---
+++ functions/helpers/getProducts.js
@@ -0,0 +1,15 @@
+const { productTable } = require('./airtable');
+const formattedReturn = require('./formattedReturn');
+module.exports = async (event) => {
+    try {
+        const products = await productTable.select().firstPage();
+        const formattedProducts = products.map((product) => ({
+            id: course.id,
+            ...course.fields,
+        }));
+        return formattedReturn(200, formattedProducts);
+    } catch (err) {
+        console.error(err);
+        return formattedReturn(500, {});
+    }
+};
