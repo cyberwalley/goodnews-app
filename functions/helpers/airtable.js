3649fb512633e6093f5ab680934f42149da14351
cyberwalley <walleyads@agmail.com>
2023-03-02 01:11:00 -0500

    remove function folders and added scroll to top

--- functions/helpers/airtable.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const Airtable = require('airtable');
-const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
-  process.env.AIRTABLE_BASE_ID,
-);
-const productTable = base(process.env.AIRTABLE_PRODUCTS_TABLE_ID);
-
-module.exports = { productTable };
